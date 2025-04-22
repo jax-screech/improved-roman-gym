f850fc862e99be96336fad454f289ad944f9a16b
jax-screech <jeromepaddy7@gmail.com>
2025-04-22 12:10:05 +0300

    worked on the class page

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 function onSubmit() {
   alert("Form submitted successfully!");
-  window.location.href = "index.html";
 }
 const mobileMenuBtn = document.getElementById('mobileMenuBtn');
 const mobileMenu = document.getElementById('mobileMenu');
 
 mobileMenuBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden');
-});
+})
+
+
+const images = document.querySelectorAll('.relative img');
+let currentIndex = 0;
+
+function showNextImage() {
+  images[currentIndex].classList.remove('opacity-100');
+  images[currentIndex].classList.add('opacity-0');
+  currentIndex = (currentIndex + 1) % images.length;
+  images[currentIndex].classList.remove('opacity-0');
+  images[currentIndex].classList.add('opacity-100');
+}
+
+images[currentIndex].classList.add('opacity-100');
+setInterval(showNextImage, 2000);
+
